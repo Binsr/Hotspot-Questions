99373af24f43cbd634012be16310c1a725fa77ff
Binsr <urosjovanovic.sna@gmail.com>
2020-04-27 17:52:46 +0200

    Update

---
+++ src/hotspotInOut/Question.java
@@ -0,0 +1,18 @@
+package hotspotInOut;
+
+public class Question {
+   
+      private String text;
+      
+      public Question() {}
+
+      public String getText() {
+         return text;
+      }
+
+      public void setText(String text) {
+         this.text = text;
+      }
+      
+      
+}
