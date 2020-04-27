1502459b5c76cc8e9f476b88359be7c3909a7161
Binsr <urosjovanovic.sna@gmail.com>
2020-04-27 15:06:13 +0200

    test

---
+++ src/hotspotInOut/Input.java
@@ -0,0 +1,18 @@
+package hotspotInOut;
+
+public class Input {
+
+   private String test;
+   
+   public Input() {}
+
+   public String getTest() {
+      return test;
+   }
+
+   public void setTest(String test) {
+      this.test = test;
+   }
+   
+   
+}
--- src/hotspotInOut/input.java
+++
@@ -1,5 +0,0 @@
-package hotspotInOut;
-
-public class input {
-
-}
