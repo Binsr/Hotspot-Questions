1f49e484625e3e9727f01cbe40ffc80c3e35f874
Binsr <urosjovanovic.sna@gmail.com>
2020-04-27 14:55:41 +0200

    First commit

---
+++ WebContent/resources/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+let canvas= document.getElementById("canvas-element");
+let ctx= canvas.getContext("2d");
+
+ctx.strokeStyle= this.color;
+ctx.lineWidth= this.lineWidth;
+ctx.strokeRect(30,30,50,50);
