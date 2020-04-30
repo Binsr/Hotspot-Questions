fe875e0661e1506db4707f155ecb62b218127067
Binsr <urosjovanovic.sna@gmail.com>
2020-04-30 14:14:23 +0200

    jsf-js connected

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,5 +1,12 @@
 
-let canvas= document.getElementById("canvas-element");
-let ctx= canvas.getContext("2d");
+/*let canvas= document.getElementById("canvas-element");
+let ctx= canvas.getContext("2d");*/
 
+/*colision_button.addEventListener("change", function(){
+	console.log("Promena");
+});
+*/
+const colisionValue= document.getElementById("j_idt6:colision_button").addEventListener("click", function(){
+	console.log("Promena");
+});
 
