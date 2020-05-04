603f7af70644f6f71b5c54737d8ade234579a229
Binsr <urosjovanovic.sna@gmail.com>
2020-05-04 16:40:26 +0200

    Update

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -6,7 +6,30 @@ let ctx= canvas.getContext("2d");*/
 	console.log("Promena");
 });
 */
-const colisionValue= document.getElementById("j_idt6:colision_button").addEventListener("click", function(){
-	console.log("Promena");
-});
+
+/* JSF - JS comunication*/
+/*const colisionValue= document.getElementById("j_idt6:colision_button").addEventListener("click", function(){
+	let tmp= document.getElementById("j_idt6:colision_value");
+	console.log(tmp.value)
+});*/
+
+/*Kod implementacije voditi racuna o id-u moze poceti i sa j_idt7:*/
+
+
+const colisionBtn= document.getElementById("j_idt6:colision_button");
+let colisionState= null;
+
+function updateColision(){
+	let colisionInput= document.getElementById("j_idt6:colision_value");
+	colisionState= colisionInput.value;
+	console.log(colisionState + " :Colision state updated~~");
+}
+
+let chosenShape= null;
+
+function updateShape(){
+	let shapeInput= document.getElementById("j_idt6:chosen_shape");
+	chosenShape= shapeInput.value;
+	console.log(chosenShape + " :Shape state updated~~");
+}
 
