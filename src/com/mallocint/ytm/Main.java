2c19c21a0e76b0b243bca4860712e1ab0bbd169b
Binsr <urosjovanovic.sna@gmail.com>
2020-05-09 20:00:32 +0200

    Render update

--- src/com/mallocint/ytm/Main.java
+++ src/com/mallocint/ytm/Main.java
@@ -3,11 +3,20 @@ package com.mallocint.ytm;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name="main")
+@ManagedBean(name = "main")
 @RequestScoped
 public class Main {
 
+   private Boolean isQuestionRendered = false;
+
    public Main() {}
 
+   public Boolean getIsQuestionRendered() {
+      return isQuestionRendered;
+   }
+
+   public void setIsQuestionRendered(Boolean isQuestionRendered) {
+      this.isQuestionRendered = !isQuestionRendered;
+   }
 
 }
